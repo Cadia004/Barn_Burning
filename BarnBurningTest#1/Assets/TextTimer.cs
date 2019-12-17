9400fc94ec311fc962a6638dd24ec6e2549690f3
Cadia004 <claudia-birth@freenet.de>
2019-12-17 12:11:39 +0100

    Add #24

---
+++ BarnBurningTest#1/Assets/TextTimer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTimer : MonoBehaviour
+{
+public float time = 5;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(time);
+Destroy(gameObject);
+    }
+
+   
+}
